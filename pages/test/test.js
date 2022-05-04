d6d107476daf2519d097a4f172df90b41bccae91
puty <13097460841>
2022-05-05 01:50:08 +0800

    Bug Fixed

--- pages/test/test.js
+++ pages/test/test.js
@@ -4,15 +4,78 @@ Page({
     console.log(e.detail.value);
   },
   data: {
-
+    subjectList:[
+      // 题目， 正确答案
+      { text: '第一题', right: 2},
+      { text: '第一题', right: 3},
+      { text: '第一题', right: 2},
+      { text: '第一题', right: 3},
+      { text: '第一题', right: 2},
+      { text: '第一题', right: 3},
+      { text: '第一题', right: 2},
+      { text: '第一题', right: 3},
+      { text: '第一题', right: 2},
+      { text: '第一题', right: 3},
+    ],
+    answerList: [0,0,0,0,0,0,0,0,0,0],
+    score: 0, // 分数
+    result: false, // 已提交?
+    message: '', // 提示信息
   },
 
   onLoad: function (options) {
 
   },
- })
-  
+
+  chooseAnswer(e){
+    let arr = this.data.answerList
+    const { field } = e.target.dataset
+    const { value } = e.detail
+    arr[field] = value
+    console.log(arr, '选中答案')
+    this.setData({
+      answerList: arr
+    })
+  },
+
+  submitAnswer() {
+    let flag = false // 是否答完
+    let finalScore = 0 // 总分
+    let finalMessage = ''
+
+    this.data.answerList.forEach(item => {
+      if(item == 0) {
+        flag = true
+        wx.showToast({
+          icon:'none',
+          title: '请回答全部题目!',
+        }) 
+      }
+    })
+    if(flag) return 
+    this.data.subjectList.forEach((item, index) => {
+      if(Number(item.right) == Number(this.data.answerList[index])){
+        finalScore += 10
+      }
+    })
+    if(finalScore >= 0 && finalScore <= 30){
+      finalMessage = '您受到电信诈骗的几率较大'
+    } else if(finalScore > 30 && finalScore <= 60) {
+      finalMessage = '您受到电信诈骗的几率中等'
+    } else {
+      finalMessage = '您受到电信诈骗的几率很小'
+    }
 
 
-  
-  
+    wx.showToast({
+      title: `您获得了${finalScore}分 ！`,
+    }) 
+    this.setData({
+      score: finalScore,
+      result: true,
+      message: finalMessage
+    })
+    console.log(finalScore, '最终成绩')
+  }
+})
+
--- pages/video/video.js
+++ pages/video/video.js
@@ -1,89 +1,73 @@
-
+const util = require("../../utils/util")
 const app = getApp()
-let content = ''
+
 Page({
 
   data: {
+    inputValue: '',
+    id: '',
+    commentList: [],
+  },
+
+  onShow(){
+    this.getComment()
   },
 
   onLoad: function (options) {
     this.setData({
       openid : app.globalData.openid,
-      })
-      
-    console.log(options.id)
-    this.data.id = options.id
-    
-    this.getDetail()
+    })
   },
-  getDetail(){
+
+  // 新增评论
+  submitComment() {
     var that = this;
-    wx.cloud.database().collection('actions').doc(this.data.id).get({
+    wx.showLoading({
+      title: '发布中',
+      mask:'true'
+    })
+    if(!this.data.inputValue){
+      wx.showToast({
+        icon:'none',
+        title: '评论为空',
+      })
+      return
+    }
+    wx.cloud.database().collection('videoComment').add({
+      data: {
+        nickName: app.globalData.userInfo.nickName,
+        faceImg: app.globalData.userInfo.avatarUrl,
+        text: this.data.inputValue,
+        time: Date.now(),
+      },
       success(res){
-
-        console.log(res)
-        var action = res.data
-        action.time = util.formatTime(new Date(action.time))
-
-        for(var l in action.prizeList){
-          if(action.prizeList[l].openid == app.globalData.openid){
-            action.isPrized = true
-          }
-        }
-
-        for(var l in action.commentList){
-          action.commentList[l].time = util.formatTime(new Date(action.commentList[l].time))
-        }
-
-        that.setData({
-          action: res.data
+        wx.showToast({
+          title: '评论发布成功！',
         })
-
+        setTimeout(() => {
+          that.getComment()
+        }, 500)
       }
     })
   },
-  getInputValue(event){
-    console.log(event.detail.value)
-    this.data.inputValue = event.detail.value
+
+  // 获取评论
+  getComment() {
+    let that = this
+    wx.cloud.database().collection('videoComment').get({
+      success(res){
+        let arr = res.data
+        arr.forEach(item => {
+          item.time = util.formatTime(new Date(item.time))
+        })
+        that.setData({
+          commentList :arr
+        })
+      }
+    })
   },
 
-  
-  publishVideoComment(){
-    var that = this;
-    console.log(that.data.id)
-    if(app.globalData.userInfo == null){
-      wx.navigateTo({
-        url: '/pages/auth/auth',
-      })
-    }else {
-
-      var that = this;
-      console.log(that.data.id)
-      wx.cloud.database().collection('actions').doc(that.data.id).get({
-        success(res){
-          console.log(res)
-          var action = res.data
-          var videocomment = {}
-          videocomment.nickName = app.globalData.userInfo.nickName
-          videocomment.faceImg = app.globalData.userInfo.avatarUrl
-          videocomment.text = that.data.inputValue
-          videocomment.time = new Date.now()
-          action.videocommentList.push(videocomment)
-          wx.cloud.database().collection('actions').doc(that.data.id).update({
-            data: {
-              videocommentList: action.videocommentList
-            },
-            success(res){
-              console.log(res)
-              wx.showToast({
-                title: '评论成功！',
-              })
-              that.getDetail()
-            }
-          })
-        }
-      })
-    }
-}
- 
+  getInputValue(event){
+    this.data.inputValue = event.detail.value
+  },
 })
